f72039e9b0be8203dff25bd6f4a76aa9b4d0b9a3
NamTran <nam.tran@zamo.io>
2022-02-07 23:03:56 +0700

    a

---
+++ redux/reducers/MemberReducer.js
@@ -0,0 +1,20 @@
+import {SIGNIN_USER_SUCCESS} from "../../constants/ActionTypes";
+
+const INIT_STATE = {
+  token: '',
+};
+
+const AuthReducer = (state = INIT_STATE, action) => {
+  switch (action.type) {
+    case SIGNIN_USER_SUCCESS: {
+      return {
+        ...state,
+        token: action.data
+      }
+    }
+    default:
+      return state;
+  }
+}
+
+export default AuthReducer;
