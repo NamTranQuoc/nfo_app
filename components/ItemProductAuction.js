5b162c86e82e1a8bb3aa8b1f6fcd9e705ed3e797
NamTran <nam.tran@zamo.io>
2022-04-26 14:11:41 +0700

    a

---
+++ components/ItemProductAuction.js
@@ -0,0 +1,45 @@
+import {Text, View} from './Themed';
+import * as React from 'react';
+import {color1} from "../constants/Colors";
+import Layout from "../constants/Layout";
+import {Image} from "react-native";
+
+const widthProduct = Layout.window.width / 2 - 20;
+const widthImage = widthProduct - 10;
+
+export default function ItemProduct({product, navigation}) {
+  return (
+      <View style={{
+        marginTop: 10,
+        marginLeft: 10,
+        marginRight: 10,
+        width: widthProduct,
+        height: widthProduct + 60,
+        borderRadius: 15,
+        borderWidth: 2,
+        borderColor: color1,
+      }}
+            onTouchEnd={() => navigation.navigate("DetailProduct")}
+      >
+        <Image style={{
+          height: widthImage,
+          width: widthImage,
+          borderRadius: 10,
+          backgroundColor: "#000",
+          alignSelf: "center",
+          marginTop: 3
+        }}
+               source={{uri: product.image}}
+
+        />
+        <Text style={{fontSize: 15, marginTop: 4, marginLeft: 5}}>{product.name}</Text>
+        <Text style={{
+          fontSize: 17,
+          marginTop: 4,
+          marginLeft: 5,
+          color: "red",
+          fontWeight: "bold"
+        }}>₫{product.price}</Text>
+      </View>
+  )
+}
